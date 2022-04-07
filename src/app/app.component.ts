f42a500ca5346444af52f31b08775e18566f0046
Vizgarra Livon <daniel.vizgarralivon@kcc.com>
2022-04-07 17:10:26 -0300

    first commit. unnable to deploy on firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TP1_daniel_vizgarra';
+  nombre:string="Daniel Vizgarra";
+  pic:string="https://umiberica.com/es/wp-content/uploads/2020/02/founder.png";
+  miJuego:string = "AHORCADO: mi juego podria ser el ahorcado con una cantidad limitada de palabras. 1 o 2 combinaciones de letras a adivinar y un numero X d intentos. con cada error modifica la imagen del jugador y asi...";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuienSoyComponent } from './quien-soy/quien-soy.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuienSoyComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
